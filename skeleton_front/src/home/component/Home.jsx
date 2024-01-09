e33f3eff742d145f24e97cba2e70131565f31cad
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-09 13:44:13 +0900

    0109

--- skeleton_front/src/home/component/Home.jsx
+++ skeleton_front/src/home/component/Home.jsx
@@ -80,27 +80,28 @@ const Home = () => {
           <div
             className="swiper-slide carousel-item-a intro-item bg-image"
             style={{ backgroundImage: "url(images/slide-3.jpg)" }}
-          />
-          <div className="overlay overlay-a"></div>
-          <div className="intro-content display-table">
-            <div className="table-cell">
-              <div className="container">
-                <div className="row">
-                  <div className="col-lg-8">
-                    <div className="intro-body">
-                      <p className="intro-title-top">
-                        Doral, Florida
-                        <br /> 78345
-                      </p>
-                      <h1 className="intro-title mb-4">
-                        <span className="color-b">204 </span> Alira
-                        <br /> Roan Road One
-                      </h1>
-                      <p className="intro-subtitle intro-price">
-                        <Link href="#">
-                          <span className="price-a">rent | $ 12.000</span>
-                        </Link>
-                      </p>
+          >
+            <div className="overlay overlay-a"></div>
+            <div className="intro-content display-table">
+              <div className="table-cell">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-8">
+                      <div className="intro-body">
+                        <p className="intro-title-top">
+                          Doral, Florida
+                          <br /> 78345
+                        </p>
+                        <h1 className="intro-title mb-4">
+                          <span className="color-b">204 </span> Alira
+                          <br /> Roan Road One
+                        </h1>
+                        <p className="intro-subtitle intro-price">
+                          <Link href="#">
+                            <span className="price-a">rent | $ 12.000</span>
+                          </Link>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -108,7 +109,6 @@ const Home = () => {
             </div>
           </div>
         </div>
-
         <div className="swiper-pagination"></div>
       </div>
       <main id="main">
