c7c9f3937de50b19d24970ffe0304451a5f3cc18
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-10 11:18:05 +0900

    0110

--- skeleton_server/user/userRouter.js
+++ skeleton_server/user/userRouter.js
@@ -4,15 +4,14 @@ const userDAO = require("./userDAO");
 
 //유저 업무와 관련된 요청이 들어왔을 때 그 요청을 처리하는 역할
 //http://localhost:8000/users/signup
-router.get("/signup", async (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   console.log("user router, singup...");
-  userDAO.signup(
-    { name: "홍길동", email: "hong@hong.com", password: "1234" },
-    (resp) => {
-      //resp  ??
-      res.send(resp);
-    }
-  );
+  //front전달 데이터 획득
+  const data = req.body;
+  userDAO.signup(data, (resp) => {
+    //resp  ??
+    res.send(resp);
+  });
 });
 
 module.exports = router;
