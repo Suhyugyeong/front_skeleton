9a230cf6138246c6e15c119d2012c2cb02e193e0
Suhyugyeong <suhyugyeongsuh@gamil.com>
2024-01-14 15:07:15 +0900

    0114

--- skeleton_front/src/App.jsx
+++ skeleton_front/src/App.jsx
@@ -1,10 +1,14 @@
+//애플리케이션의 주요 컴포넌트 또는 레이아웃을 정의
+//애플리케이션의 루트 컴포넌트, 일반적으로 다른 하위 컴포넌트를 포함하고 구성
+//전반적인 구조와 레이아웃을 결정
 import { Routes, Route } from "react-router-dom";
 import Header from "./home/component/Header";
 import Footer from "./home/component/Footer";
 import HomeMain from "./home/HomeMain";
 import UserMain from "./user/UserMain";
-import BoardMain from "./board/BoardMain"; //여기
+import BoardMain from "./board/BoardMain";
 
+//페이지 레이아웃 및 라우팅을 설정하는 함수형 컴포넌트
 const App = () => {
   return (
     <div>
@@ -16,7 +20,9 @@ const App = () => {
       즉, XXXMain만 이곳에서 등록하고 XXX 업무에 의한 화면 라우팅은 XXXMain에 명시*/}
       <Routes>
         <Route path="/" element={<HomeMain />} />
+        {/* element는 렌더링할 컴포넌트 여기서 HomeMain.jsx */}
         <Route path="/user/*" element={<UserMain />} />
+        {/* 여기서 별은 와일드카드 매개변수를 나타냄. /user/ 뒤에 어떤 문자열이든 와일드카드에 해당하는 위치에 올 수 있다 */}
         <Route path="/board/*" element={<BoardMain />} />
       </Routes>
       <Footer />
--- skeleton_front/src/main.jsx
+++ skeleton_front/src/main.jsx
@@ -1,3 +1,6 @@
+//일반적으로 애플리케이션을 시작하고 필요한 리소스를 로드하며 필요한 설정을 수행
+//애플리케이션의 진입점, 보통 초기화 코드나 전역적인 설정이 이루어짐
+//ReactDOM.render() 함수를 사용하여 App.jsx나 다른 주요 컴포넌트를 렌더링!!
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -13,9 +16,12 @@ import "./assets/vendor/swiper/swiper-bundle.min.css";
 import App from "./App.jsx";
 import "./index.css";
 
+//리액트 애플리케이션을 초기화하고 렌더링하는데 사용
+//루트 리액트 렌더러 생성
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* React Router의 핵심 컴포넌트 중 하나 */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
